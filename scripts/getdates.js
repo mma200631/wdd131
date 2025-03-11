af34e2ad7efd2421cfd9e2d4820ae6ce749739fe
mma200631 <assumpta.chidinma06@gmail.com>
2025-03-11 21:00:40 +0100

    week01 assignment: Homepage

---
+++ scripts/getdates.js
@@ -0,0 +1,4 @@
+document.getElementById("currentYear").textContent = new Date().getFullYear();
+
+    // Set the last modified date
+    document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
